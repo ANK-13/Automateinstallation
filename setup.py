3c233ee08f737314613f971e8cf666dae5f76189
aniketbaad <aniketbaad@gmail.com>
2019-04-25 13:00:25 +0530

    final setup.py

--- setup.py
+++ setup.py
@@ -18,17 +18,36 @@ response=install("sudo apt-get install -y --force-yes python3-pip")
 if(response[0] != 0):
         print("could not install python3-pip")
 
+response = install("sudo apt-get install -y --force-yes nmap")
+if(response[0] != 0):
+	print("Could not install nmap")
+
 response = install("sudo apt-add-repository -y ppa:ansible/ansible")
 if(response[0] != 0):
-    print("Could not install software")
+    print("Could not install software-installer")
 
 response = install("sudo apt-get update")
 if(response[0] != 0):
-   print("Could not install software")
+    print("Could not install software")
 
 response = install("sudo apt-get -y --force-yes install ansible")
 if(response[0] != 0):
-    print("Could not install Ansible")
+    print("Could not install software-installer")
 
-print(response)
+response = install("sudo pip3 install virtualenv")
+if(response[0] != 0):
+    print("Could not install virtualenv")
+	
+reponse = install("sudo virtualenv -p /usr/bin/python3 env")
+if(response[0] != 0):
+	print("could not create a virtual env")
+
+response = install("/root/project/Automateinstallation/env/bin/pip3 install -r requirements.txt")
+if(response[0] != 0):
+        print("could not install requirements file")
+
+response = install("/root/project/Automateinstallation/env/bin/python3 app.py")
+if(response[0] != 0):
+        print("could not run project")
+
 
